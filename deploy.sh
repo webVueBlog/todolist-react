d220e1060e876619067d4c93ce6a44c67ded31f8
webVueBlog <2979986590@qq.com>
2022-05-06 01:40:54 +0800

    feat: shell

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ set -e
 yarn build
 printf "打包成功\n"
 
-cd dist
+cd build
 
 git init
-git checkout -b dist
+git checkout -b build
 git add -A
 git commit -m 'deploy'
 printf "本地提交成功\n"
-git push -f git@github.com:webVueBlog/todolist-react.git dist
+git push -f git@github.com:webVueBlog/todolist-react.git build
 
 printf "dist目录上传成功\n"
